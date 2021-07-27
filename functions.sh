b1dbccd277dd39faec7a7fa89febd404ada7772a
Charles Sprayberry <me@cspray.io>
2021-07-27 19:49:23 -0400

    More improvements for sanity check

--- functions.sh
+++ functions.sh
@@ -3,11 +3,33 @@ print_success_mark() {
 }
 
 print_failure_mark() {
-  echo -e "\e[31m\u2716\e[0m"
+  print_error "\u2716"
 }
 
-os_info() {
-  declare -A os_info_map
+print_error() {
+  echo -e "\e[31m$1\e[0m"
+}
 
-  echo "${os_info_map[*]}"
+is_arch_linux() {
+  while read -r line; do
+    IFS='=' read -ra key_value <<< "$line"
+    key=${key_value[0]}
+    value=${key_value[1]}
+
+    if [[ $key == 'ID' ]] || [[ $key == 'ID_LIKE' ]]; then
+      if [[ $value == 'arch' ]]; then
+        return 0
+      fi
+    fi
+  done < /etc/os-release
+
+  return 1
+}
+
+is_kde() {
+  if [[ ${XDG_CURRENT_DESKTOP,,} == "kde" ]]; then
+    return 0
+  else
+    return 1
+  fi
 }
